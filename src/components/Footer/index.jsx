29ee542cfaf7caec7a1e9a698468affea6781969
AbigailSalazar <75355095+AbigailSalazar@users.noreply.github.com>
2025-01-05 13:00:53 -0700

    Update footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,18 @@
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 
 function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <img className={styles.logo} src="/logo.svg" alt="AluraFlix logo"/>
-        </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+      <img className={styles.logo} src="/logo.svg" alt="AluraFlix logo" />
+      <section className={styles.buttons_container}>
+        <button className={styles.home_button}>
+          <img src="/src/assets/home.svg" alt="home" /> HOME
+        </button>
+        <button>
+          <img src="/src/assets/plus.svg" alt="plus" />
+        </button>
+      </section>
+    </footer>
+  );
 }
 export default Footer;
