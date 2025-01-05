16db5b06f4b8bf5f025bf0ab0c084c7f2f4c7055
AbigailSalazar <75355095+AbigailSalazar@users.noreply.github.com>
2025-01-05 11:29:27 -0700

    Update Banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -8,21 +8,23 @@ function Baneer() {
         src="img/Banner.png"
         alt="backgound banner"
       />
-      <div className={styles.bannerContent}>
-        <div className={styles.info}>
-          <h1 className={styles.title}>FRONT END</h1>
-          <h2 className={styles.subtitle}>Challenge React</h2>
-          <p className={styles.description}>
-            Este challenge es una forma de aprendizaje. Es un mecanismo donde
-            podrás comprometerte en la resolución de un problema para poder
-            aplicar todos los conocimientos adquiridos en la formación React.
-          </p>
+      <div className={styles.overlay}>
+        <div className={styles.bannerContent}>
+          <div className={styles.info}>
+            <h1 className={styles.title}>FRONT END</h1>
+            <h2 className={styles.subtitle}>Challenge React</h2>
+            <p className={styles.description}>
+              Este challenge es una forma de aprendizaje. Es un mecanismo donde
+              podrás comprometerte en la resolución de un problema para poder
+              aplicar todos los conocimientos adquiridos en la formación React.
+            </p>
+          </div>
+          <img
+            className={styles.image}
+            src="img/player.png"
+            alt="¿Qué significa pensar como programador?"
+          />
         </div>
-        <img
-          className={styles.image}
-          src="img/player.png"
-          alt="¿Qué significa pensar como programador?"
-        />
       </div>
     </div>
   );
