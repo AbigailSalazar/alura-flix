19066c0b3838f37c3667e7e10e5543f66a042da1
AbigailSalazar <75355095+AbigailSalazar@users.noreply.github.com>
2025-01-05 13:31:51 -0700

    Update page NuevoVideo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,17 @@
 import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <img className={styles.logo} src="/logo.svg" alt="AluraFlix logo" />
       <section className={styles.buttons_container}>
-        <button className={styles.home_button}>
+        <Link to="/" className={styles.home_button}>
           <img src="/src/assets/home.svg" alt="home" /> HOME
-        </button>
-        <button>
+        </Link>
+        <Link to="/nuevo">
           <img src="/src/assets/plus.svg" alt="plus" />
-        </button>
+        </Link>
       </section>
     </footer>
   );
