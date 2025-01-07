470a645cb42abcbc95531409b54d6ad07b92f9d1
AbigailSalazar <75355095+AbigailSalazar@users.noreply.github.com>
2025-01-06 18:51:35 -0700

    Update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,23 @@
 import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
     <header className={styles.header}>
       <a href="/">
         <img src="/logo.svg" alt="AluraFlix logo" />
       </a>
       <div className={styles.link_container}>
-        <Link className={styles.link} to="/">
+        <Link className={pathname == "/" && styles.link_actual} to="/">
           HOME
         </Link>
-        <Link className={styles.link} to="/nuevo">
+        <Link
+          className={pathname == "/nuevo" && styles.link_actual}
+          to="/nuevo"
+        >
           NUEVO VIDEO
         </Link>
       </div>
